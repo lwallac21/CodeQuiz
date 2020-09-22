faf178aed65172f4d4cd7b857d2bca29c20df383
Lawrence Wallace <lwallac21@gmail.com>
2020-09-22 07:41:12 -0400

    I'm thinking this is done

--- Assets/script.js
+++ Assets/script.js
@@ -1,22 +1,30 @@
 //Variable declarations
 const startButton = $("#start-button");
 const nextButton = $("#next-button");
+const endButton = $("#end-button")
 let shuffleQuestions, questionIndex;
 const questionField = $("#question");
 const answersButtons = $("answer-box");
+let highscores = JSON.parse(window.localStorage.getItem("highscores")) || []
+const maxHighscores = 5
+let scoresList = document.getElementById("scores-list");
 let scoreName = $("#score-name");
-let score = "";
-let currentScore = 0;
+let showScore
+let score = 0;
+let currentScore;
 let timerCount;
 let playerName;
+let answerBtn;
 
 //Click events for the 2 main buttons
 startButton.on("click", startQuiz);
-nextButton.on("click", function(){
+nextButton.on("click", function () {
     questionIndex++
     initializeQuestion();
     quizScore();
+    console.log(currentScore)
 });
+endButton.on("click", writeScore)
 
 // Start quiz button function
 function startQuiz() {
@@ -26,68 +34,101 @@ function startQuiz() {
         return;
     }
 
-//start quizTimer call and quizScore call
+    //start quizTimer call and quizScore call
     quizTimer()
 
-//hide and show the necessary buttons
+    //hide and show the necessary buttons
     $("#start-button").addClass("d-none");
     $("#question-box").removeClass("d-none");
     $("#player-name").addClass("d-none")
-    scoreName.text(playerName.text)
+    $("#display-name").text(playerName.text)
     shuffleQuestions = questions;
     questionIndex = 0;
     nextQuestion();
 };
 
 //quiz Timer Function
- function quizTimer() {
-        timerCount = 100;
-        countDown = setInterval(function () {
-        timerCount --;
+function quizTimer() {
+    timerCount = 100;
+    countDown = setInterval(function () {
+        timerCount--;
         if (timerCount <= 0) {
             clearInterval(countDown);
-            alert("GAME OVER!");
+            alert("You have run out of time!");
         }
         document.getElementById("time-remaining").innerHTML = timerCount + " seconds";
     }, 1000);
-    
- }
-
- function quizScore() {
-    let showScore = currentScore
-    currentScore = score * timerCount
-    
-     
-     $("#score-box").text(currentScore+showScore)
-
-
- }
-
- //For next and start buttons, gets next question object in questions
-function nextQuestion() {
-    showQuestion(shuffleQuestions[questionIndex])
 
 }
+
+function quizScore() {
+    currentScore = score * timerCount
+    showScore = currentScore + currentScore
+    showScore+=currentScore
+
+    $("#score-box").text(showScore)
+
+
+}
+
+//For next and start buttons, gets next question object in questions
+function nextQuestion() {
+    if (questionIndex < 9) {
+        showQuestion(shuffleQuestions[questionIndex])
+    }
+    else hideAll();
+}
+
+//End of quiz hide all!
+function hideAll() {
+    $("#answer-box").addClass("d-none")
+    $("#question-box").addClass("d-none")
+    $("next-button").addClass("d-none")
+    $("#end-button").removeClass("d-none")
+    $("#controller").text("Your final score is " + currentScore)
+    clearInterval(countDown);
+}
+
+function writeScore() {
+    event.preventDefault()
+    let newScore = {
+        name: playerName,
+        score: showScore
+    }
+    highscores.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    $("#quiz").addClass("d-none");
+    $("#high-score").removeClass("d-none");
+    highscores.sort((a,b) =>  b.score - a.score)
+
+    var newArray = highscores.slice(0,5);
+    
+    highscores.push("highscores", JSON.stringify(highscores))
+    
+    scoresList.innerHTML= newArray.map(score => {
+        return `<li class="list-group-item">${score.name}-${score.score}</li>`
+
+    }).join("");
+    
+    
+}
+
+
 
 //function for getting the next question object inside the questions array
 function showQuestion(question) {
     questionField.text(question.question)
     question.answers.forEach(answer => {
-        const answerBtn = $("<button>");
-        answerBtn.addClass('btn, btn-primary');
+        answerBtn = $("<button>");
+        answerBtn.addClass('answerbtn btn btn-primary');
         answerBtn.text(answer.text);
 
         if (answer.correct) {
             answerBtn.attr("dataset", answer.correct);
 
         }
-        if (questionIndex >= questions.length - 1) {
-            alert("Game over! Your final score was: ", currentScore)
-            return
-        }
         answerBtn.on("click", selectAnswer);
         $("#answer-box").append(answerBtn);
-
     });
 
 
@@ -104,10 +145,10 @@ function initializeQuestion() {
 function selectAnswer(event) {
     let selectedAnswer = event.target;
     let rightAnswer = selectedAnswer.hasAttribute("dataset");
-    giveBtnClass(selectedAnswer, rightAnswer)
-console.log(score)
-
-}
+    giveBtnClass(selectedAnswer, rightAnswer);
+    document.querySelector(".answerbtn").disabled = true;
+    
+};
 
 //changes classes to show correct, adds score
 function giveBtnClass(element, correct) {
@@ -115,11 +156,11 @@ function giveBtnClass(element, correct) {
     nextButton.removeClass("d-none")
     if (correct) {
         element.classList.add("btn-success");
-        score += 1
+        score = score + 1;
     }
     else {
         element.classList.add("btn-danger");
-        timerCount-= 10
+        timerCount -= 10
     }
 
 }
@@ -196,21 +237,21 @@ const questions = [
         ]
     },
     {
-        question: "What is the Jquery selector key symbol?",
+        question: "What does JSON stand for?",
         answers: [
-            { text: '$', correct: true },
-            { text: "#", correct: false },
-            { text: "?", correct: false },
-            { text: "+", correct: false },
+            { text: 'JavaScript Object Notation', correct: true },
+            { text: "Java Object Notation", correct: false },
+            { text: "Just start object now", correct: false },
+            { text: "Jason Statham's Oscar Nomination", correct: false },
         ]
     },
     {
-        question: "What is the Jquery selector key symbol?",
+        question: "How often should you commit to github when working on a javascript project",
         answers: [
-            { text: '$', correct: true },
-            { text: "#", correct: false },
-            { text: "?", correct: false },
-            { text: "+", correct: false },
+            { text: 'Very often!', correct: true },
+            { text: "When completed", correct: false },
+            { text: "Never. Just push it, first time, every time.", correct: false },
+            { text: "Don't know.", correct: false },
         ]
     }
 ]
